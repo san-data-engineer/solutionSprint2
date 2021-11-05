c481de376a473a9d83eeec9f851650b823477768
san-data-engineer <93750367+san-data-engineer@users.noreply.github.com>
2021-11-05 01:09:40 -0300

    Create README.cmd

---
+++ README.cmd
@@ -0,0 +1,12 @@
+# Solution Sprint 2
+
+Repositório criado para a solution sprint 2 do curso de engenharia de dados (4ABDO). 
+
+Aluno:
+* Alessandro Bezerra
+
+
+## Objetivo
+Nosso objetivo será planejar e implementar a arquitetura de dados poliglota para tratamento do problema de performance na funcionalidade de ranking, da plataforma utilizada pela empresa Engage.
+
+## Índice
